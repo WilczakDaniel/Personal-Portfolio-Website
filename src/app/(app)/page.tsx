07f24654db0604b75a4cc5a2f5b31c22f78d51dc
WilczakDaniel <92593009+WilczakDaniel@users.noreply.github.com>
2024-10-21 12:32:02 +0200

    Edit

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -14,24 +14,7 @@ const Home = () => {
     return (
         <>
             <main className={`${styles.homepage}`}>
-                <Sidebar side="right">
-                    <SidebarLink ariaLabel="Mailto rafalnawojczyk@gmail.ocm" href="mailto:rafalnawojczyk@gmail.com">
-                        <p className={styles['homepage__sidebar-email']}>rafalnawojczyk@gmail.com</p>
-                    </SidebarLink>
-                </Sidebar>
-                <Sidebar side="left">
-                    <SidebarLink
-                        ariaLabel="Github link to rafalnawojczyk account - opens in new tab"
-                        href="https://github.com/rafalnawojczyk"
-                    >
-                        <GitHubLogo className={styles.homepage__logo} />
-                    </SidebarLink>
-                </Sidebar>
-                <Hero />
-                <About />
-                <Projects />
-                <Contact />
-                <Footer />
+               <div>DW Code</div>
             </main>
         </>
     );
